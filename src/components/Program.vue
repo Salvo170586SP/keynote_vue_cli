55ba41277ae4b778087a41073f84d4e97f2a90c9
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-06-29 13:39:35 +0200

    added Program

---
+++ src/components/Program.vue
@@ -0,0 +1,72 @@
+<template>
+  <section id="program">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md">
+          <h1>Program</h1>
+        </div>
+        <div class="col-12 col-md">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque
+            ipsam ad iste animi facilis nobis cum aliquam reiciendis vero
+          </p>
+        </div>
+        <div class="col-12 col-md text-end">
+          <a href="#">View Full Program</a>
+        </div>
+        <div class="col-12 p-0 py-3 text-light">
+          <div class="bg-danger py-3 w-100 container-fluid">
+            <div class="row">
+              <div class="col-12 col-md-2">
+                <h5>DAY 1</h5>
+                <span>23 May 2016</span>
+              </div>
+              <div class="col-12 col-md-2">
+                <h5>DAY 2</h5>
+                <span>23 May 2016</span>
+              </div>
+              <div class="col-12 col-md-2">
+                <h5>DAY 3</h5>
+                <span>23 May 2016</span>
+              </div>
+              <div class="col-12 col-md-2">
+                <h5>DAY 4</h5>
+                <span>23 May 2016</span>
+              </div>
+              <div class="col-12 col-md-2">
+                <h5>DAY 5</h5>
+                <span>23 May 2016</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-3">
+            <ul class="list-group">
+                <li class="list-group-item text-muted"> <i class="fa-solid fa-clock me-2"></i> 9.30 - 10.30</li>
+                <li class="list-group-item text-muted"><i class="fa-solid fa-location-arrow text-muted me-2"></i> ROOM A</li>
+                <li class="list-group-item text-danger"><i class="fa-solid fa-user-large text-muted me-2"></i> LAURENCE FRANCIS</li>
+            </ul>
+        </div>
+        <div class="col-12 col-md-9">
+            <h4>WELCOMING AND INTRODUCING</h4>
+          <p class="text-muted py-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            placeat doloribus quasi non perspiciatis, excepturi dolor commodi
+            facere explicabo quo repellat delectus aut! Dolore placeat fugiat
+            veniam beatae aut velit.
+          </p>
+          <img src="../assets/image/speaker-8-150x150.jpg" width="50" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Program",
+};
+</script>
+
+<style scoped lang="scss">
+</style>
