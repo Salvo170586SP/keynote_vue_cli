d01bff51c289e96ae792400727f79c800873fcb9
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-06-29 10:24:55 +0200

    added WhoWeAre

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,17 +2,20 @@
   <div>
     <Header />
     <Jumbotron />
+    <WhoWeAre />
   </div>
 </template>
 
 <script >
 import Header from "./Header.vue";
 import Jumbotron from "./Jumbotron.vue";
+import WhoWeAre from "./WhoWeAre.vue";
 export default {
   name: "Home",
   components: {
     Header,
-    Jumbotron
+    Jumbotron,
+    WhoWeAre
   },
 };
 </script>
---
+++ src/components/WhoWeAre.vue
@@ -0,0 +1,44 @@
+<template>
+  <section id="who-we-are" class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col text-end text-muted"><i class="fa-solid fa-trophy"></i></div>
+        <div class="col-3">
+          <h6>WHO WE ARE?</h6>
+          <p class="text-muted pt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ipsa
+            aperiam, sunt doloribus accusamus neque esse placeat dignissimos,
+          </p>
+          <a href="#" class="nav-link p-0 text-danger fst-italic">Learn more</a>
+        </div>
+        <div class="col text-end text-muted"><i class="fa-solid fa-bolt-lightning"></i></div>
+        <div class="col-3">
+          <h6>WHAT WE DO?</h6>
+          <p class="text-muted pt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ipsa
+            aperiam, sunt doloribus accusamus neque esse placeat dignissimos,
+          </p>
+          <a href="#" class="nav-link p-0 text-danger fst-italic">Learn more</a>
+        </div>
+        <div class="col text-end text-muted"><i class="fa-solid fa-question"></i></div>
+        <div class="col-3">
+          <h6>WHY US?</h6>
+          <p class="text-muted pt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ipsa
+            aperiam, sunt doloribus accusamus neque esse placeat dignissimos,
+          </p>
+          <a href="#" class="nav-link p-0 text-danger fst-italic">Learn more</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "WhoWeAre",
+};
+</script>
+
+<style scoped lang="scss">
+</style>
