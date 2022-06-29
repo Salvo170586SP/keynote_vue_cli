fb55bb9ea3d5eb4b778790198f04938f630a8e33
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-06-29 11:18:57 +0200

    fixed col

--- src/components/WhoWeAre.vue
+++ src/components/WhoWeAre.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="row">
         <div class="col text-end text-muted"><i class="fa-solid fa-trophy"></i></div>
-        <div class="col-3">
+        <div class="col-12 col-md-3">
           <h6>WHO WE ARE?</h6>
           <p class="text-muted pt-2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ipsa
@@ -12,7 +12,7 @@
           <a href="#" class="nav-link p-0 text-danger fst-italic">Learn more</a>
         </div>
         <div class="col text-end text-muted"><i class="fa-solid fa-bolt-lightning"></i></div>
-        <div class="col-3">
+        <div class="col-12 col-md-3">
           <h6>WHAT WE DO?</h6>
           <p class="text-muted pt-2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ipsa
@@ -21,7 +21,7 @@
           <a href="#" class="nav-link p-0 text-danger fst-italic">Learn more</a>
         </div>
         <div class="col text-end text-muted"><i class="fa-solid fa-question"></i></div>
-        <div class="col-3">
+        <div class="col-12 col-md-3">
           <h6>WHY US?</h6>
           <p class="text-muted pt-2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ipsa
