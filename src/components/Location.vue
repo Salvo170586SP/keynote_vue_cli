972c11e9b234643df9e4c0e06f37c4b624ccc8f7
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-06-29 15:27:46 +0200

    added location (beta)

---
+++ src/components/Location.vue
@@ -0,0 +1,34 @@
+<template>
+  <section id="location">
+    <div class="container h-100">
+        <div class="row h-100 align-items-center">
+            <div class="col-12 d-flex flex-column justify-content-center align-items-center text-light">
+                <h2>LOCATION</h2>
+                <h2 class="text-center">GD BUILDING, GRAND CONFERENCE ROOM</h2>
+                <p>11 GD Building, Rivr Mal, NewYork, USA 8879</p>
+                <p>Check For Accomodation Here</p>
+                <p class="b w-50 text-center">+1-255-334-5844  Contact@TheKeynoteTheme.com</p>
+            </div>
+        </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name:"Location"
+}
+</script>
+
+<style scoped lang="scss">
+#location{
+    height: 50vh;
+    background-image: url('../assets/image/location-bg.jpg');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    .b{
+        border-top: 1px solid white;
+    }
+}
+</style>
